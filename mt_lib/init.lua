f832f891d8f3f61a4c7d673604aaffcbb50d12f7
MRguldDEV <139634720+McMRguldDev@users.noreply.github.com>
2026-08-21 13:14:22 +0200

    Updated init.lua
    Added logging functionality and improved load messages.

--- mt_lib/init.lua
+++ mt_lib/init.lua
@@ -1,4 +1,13 @@
 mt.lib = {}
-mt.cache = {}
 
-print('[mt_lib] loaded')
+local function log(message)
+    print(('[mt_lib] %s'):format(message))
+end
+
+if mt.config.txAdminClean then
+    log('Loaded with txAdmin cleanup enabled')
+else
+    log('Loaded')
+end
+
+mt.lib.initialized = true
